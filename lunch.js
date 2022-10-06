31a6866887339420aa0293d2732f2800b513151e
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2022-10-06 11:04:12 +0200

    fsdsa

---
+++ lunch.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+const weekday = ("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag");
+const d = new Date();
+let day = weekday[getDay()]
+
+
+
+const body = document.querySelector("body");
+
+body.innerHTML = day
+
+
+
+
+//torsdagar - ärtsoppa m. pannkakor
+//fredagar - tacos
+//lördag, söndag - Stängt
